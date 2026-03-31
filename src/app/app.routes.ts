73c2d11fb4a5bd7b4e0ec1e9f85c7db89ce16329
caco83100 <31388813+caco83100@users.noreply.github.com>
2026-03-31 14:34:00 +0200

    feat : add tier list route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { Body } from './body/body';
 import { Characters } from './characters/characters';
 import { Locations } from './locations/locations';
 import { About } from './about/about';
+import { TierList } from './tier-list/tier-list';
 
 export const routes: Routes = [
      { 'path': '', 'component': Body },
      {'path': 'characters', 'component': Characters },
      {'path': 'locations', 'component': Locations },
      {'path': 'about', 'component': About },
+     {'path': 'tierlist', 'component': TierList},
 ];
---
+++ src/app/tier-list/tier-list.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tier-list',
+  imports: [],
+  templateUrl: './tier-list.html',
+  styleUrl: './tier-list.css',
+})
+export class TierList {}
