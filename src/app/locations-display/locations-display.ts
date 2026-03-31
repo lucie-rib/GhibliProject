c445ff6a7fbc4063f8420494695cab84136caa9b
caco83100 <31388813+caco83100@users.noreply.github.com>
2026-03-31 10:35:31 +0200

    feat : fonctional locations page

--- src/app/locations-display/locations-display.ts
+++ src/app/locations-display/locations-display.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { FilmCard } from '../film-card/film-card';
 
 @Component({
   selector: 'app-locations-display',
-  imports: [],
+  imports: [FilmCard],
   templateUrl: './locations-display.html',
   styleUrl: './locations-display.css',
 })
-export class LocationsDisplay {}
+export class LocationsDisplay {
+  @Input() location: any;
+  @Output() locationClicked = new EventEmitter<string>();
+
+  onLocationClicked() {
+    console.log('Location clicked:', this.location.name);
+    this.locationClicked.emit(this.location.name);
+  }
+}
