6e221d9bcdafb807140dd920f6249c229d9891f9
caco83100 <31388813+caco83100@users.noreply.github.com>
2026-03-31 16:25:51 +0200

    feat : add dragable card tierlist

--- src/app/tier-list/tier-list.ts
+++ src/app/tier-list/tier-list.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { CarouselCard } from '../carousel-card/carousel-card';
 import { Data } from '../data';
 
